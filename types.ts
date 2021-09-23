291cf5184187f35d6561adc12c000e94b8fd3828
LaloHao <lalohao@gmail.com>
2021-09-23 13:39:33 -0600

    Introductory steps to repository boilerplate.

---
+++ types.ts
@@ -0,0 +1,26 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Movie = {
+  __typename?: 'Movie';
+  actors?: Maybe<Array<Maybe<Person>>>;
+  released?: Maybe<Scalars['Int']>;
+  title: Scalars['String'];
+};
+
+export type Person = {
+  __typename?: 'Person';
+  born?: Maybe<Scalars['Int']>;
+  movies?: Maybe<Array<Maybe<Movie>>>;
+  name: Scalars['String'];
+};
